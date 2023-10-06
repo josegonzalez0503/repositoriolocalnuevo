5046b7899c2797f959fd5d2d6bc51d02f0513770
Jose <josemgagonzalez@gmail.com>
2023-10-06 17:51:14 +0200

    afegit el nom de l'autor

--- Documents/ControlVersions/Tema2.java
+++ Documents/ControlVersions/Tema2.java
@@ -2,14 +2,13 @@ public class Tema2{
 
 	public static void main(String[] args) {
 	
-		//Esto es el hola mundo
+	
 		System.out.println("Hola Mundo!!");
 	    
-		 * 
-		 */
+	
 		System.out.println("Jose");	
 		
-//TODO comprobación ejercicio
+
 	}
  
 }
