c68053b8328c514629aad24c212c2d9977cb36f5
Jose <josemgagonzalez@gmail.com>
2023-10-06 17:47:05 +0200

    Estat inicial del programa

---
+++ Documents/ControlVersions/Tema2.java
@@ -0,0 +1,15 @@
+public class Tema2{
+
+	public static void main(String[] args) {
+	
+		//Esto es el hola mundo
+		System.out.println("Hola Mundo!!");
+	    
+		 * 
+		 */
+		System.out.println("Jose");	
+		
+//TODO comprobación ejercicio
+	}
+ 
+}
