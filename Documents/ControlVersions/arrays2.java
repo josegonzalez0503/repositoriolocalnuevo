afce5d40594c735d1fe3ec3ac6237963de5254de
Jose <josemgagonzalez@gmail.com>
2023-10-10 16:08:58 +0200

    Este es el commit

---
+++ Documents/ControlVersions/arrays2.java
@@ -0,0 +1,13 @@
+package ejrciciotema3;
+
+public class arrays1 {
+//comentario del ejercicio
+	public static void main(String[] args) {
+		int[] arrayEnteros = {0, 3, 3, 7, 5, 9, 1, 1, 1, 1};
+		System.out.println("array[1]= "+arrayEnteros[1]);
+		System.out.println("array[3]= "+arrayEnteros[3]);
+		System.out.println("array[5]= "+arrayEnteros[5]);
+		System.out.println("array[6]= "+arrayEnteros[5]);
+	}
+
+}
