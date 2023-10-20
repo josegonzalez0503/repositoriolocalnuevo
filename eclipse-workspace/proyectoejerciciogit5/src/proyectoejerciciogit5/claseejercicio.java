604de346adf3d424079c51f48d92f089d8a8cdf3
Jose <josemgagonzalez@gmail.com>
2023-10-20 16:43:36 +0200

    commit ejercicio

--- eclipse-workspace/proyectoejerciciogit5/src/proyectoejerciciogit5/claseejercicio.java
+++ eclipse-workspace/proyectoejerciciogit5/src/proyectoejerciciogit5/claseejercicio.java
@@ -3,8 +3,9 @@ package proyectoejerciciogit5;
 public class claseejercicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("Ejercicio git");
 	}
 
 }
